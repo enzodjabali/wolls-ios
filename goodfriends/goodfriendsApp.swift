da090ec649ef059c0c3674f6adda23e13a5381c9
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-05-22 16:45:27 +0200

    Initial Commit

---
+++ goodfriends/goodfriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodfriendsApp.swift
+//  goodfriends
+//
+//  Created by Enzo on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct goodfriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
