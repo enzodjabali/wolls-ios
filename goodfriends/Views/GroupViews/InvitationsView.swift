bea72e00adb4f4d32117cb945dd59f269a708c94
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-05-25 17:43:52 +0200

    feat(#10): making the invitations look more native

--- goodfriends/Views/GroupViews/InvitationsView.swift
+++ goodfriends/Views/GroupViews/InvitationsView.swift
@@ -1,82 +1,96 @@
 import SwiftUI
 
-struct InvitationRow: View {
-    let group: Group
-    let acceptAction: () -> Void
-    let declineAction: () -> Void
-    
-    @State private var feedback: String = ""
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            
-            Text(group.name)
-                .font(.headline)
-            Text(group.description)
-                .font(.subheadline)
-                .foregroundColor(.gray)
-            HStack {
-                Button(action: acceptAction) {
-                    Text("Accept")
-                        .bold() // Make text bold
-                }
-                Button(action: declineAction) {
-                    Text("Decline")
-                        .bold() // Make text bold
-                        .foregroundColor(.red) // Make text red
-                }
-            }
-        }
-        
-    }
-}
-
 struct InvitationsView: View {
     @ObservedObject var groupMembershipController = GroupMembershipController.shared
     @State private var invitations: [Group] = []
     
     var body: some View {
-        VStack {
-         
-            if invitations.isEmpty {
-                Text("No invitations")
-                    .padding()
-            } else {
-                List(invitations, id: \.id) { group in
-                    InvitationRow(group: group, acceptAction: {
-                        self.respondToInvitation(groupId: group.id, accept: true)
-                    }, declineAction: {
-                        self.respondToInvitation(groupId: group.id, accept: false)
-                    })
+        NavigationView {
+            List(invitations, id: \.id) { group in
+                InvitationRow(group: group) { accept in
+                    // Respond to invitation
+                    self.respondToInvitation(group: group, accept: accept)
                 }
             }
-        }
-        .onAppear {
-            fetchInvitations()
+            .navigationBarTitle("Invitations")
+            .onAppear {
+                // Fetch invitations when the view appears
+                self.fetchInvitations()
+            }
         }
     }
     
-    func fetchInvitations() {
+    private func fetchInvitations() {
         groupMembershipController.fetchInvitations { result in
             switch result {
             case .success(let groups):
                 self.invitations = groups
             case .failure(let error):
+                // Handle error
                 print("Error fetching invitations: \(error.localizedDescription)")
             }
         }
     }
     
-    func respondToInvitation(groupId: String, accept: Bool) {
-        groupMembershipController.respondToInvitation(groupId: groupId, accept: accept) { result in
+    private func respondToInvitation(group: Group, accept: Bool) {
+        groupMembershipController.respondToInvitation(groupId: group.id, accept: accept) { result in
             switch result {
             case .success:
-                // Invitation response successful, update UI or perform any necessary action
-                fetchInvitations() // Refresh invitations after responding
+                // Refresh invitations after responding
+                self.fetchInvitations()
             case .failure(let error):
-                print("Error responding to invitation: \(error.localizedDescription)")
                 // Handle error
+                print("Error responding to invitation: \(error.localizedDescription)")
             }
         }
     }
 }
+
+struct InvitationRow: View {
+    let group: Group
+    let respondAction: (Bool) -> Void
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("You've been invited you to join ")
+                    .font(.body)
+                    + Text(group.name)
+                    .font(.body)
+                    .bold()
+                
+                HStack(spacing: 20) {
+                    Button(action: {
+                        self.respondAction(true)
+                    }) {
+                        Text("Accept")
+                            .foregroundColor(Color.white)
+                            .padding(4)
+                            .frame(width: 80)
+                            .background(Color.blue)
+                            .cornerRadius(20)
+                    }
+                    .padding(.top, 5) // Add padding above the button
+
+                    Button(action: {
+                        self.respondAction(false)
+                    }) {
+                        Text("Decline")
+                            .foregroundColor(Color.blue)
+                            .padding(4)
+                            .frame(width: 80)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.blue, lineWidth: 2)
+                            )
+                            .background(Color.white)
+                            .cornerRadius(20)
+                    }
+                    .padding(.top, 5) // Add padding above the button
+                }
+            }
+            Spacer() // Add spacer to push buttons to the left
+        }
+    }
+}
+
