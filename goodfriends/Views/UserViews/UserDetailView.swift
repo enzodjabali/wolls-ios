232f0455265b191771544c2774848e50fcb6c455
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-06-12 12:05:26 +0300

    feat(#19): adding a close button on the user view

--- goodfriends/Views/UserViews/UserDetailView.swift
+++ goodfriends/Views/UserViews/UserDetailView.swift
@@ -2,38 +2,54 @@ import SwiftUI
 
 struct UserDetailView: View {
     var user: User
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack(spacing: 16) {
-            AvatarView(initials: userInitials(user))
-                .frame(width: 100, height: 100)
-                .background(
-                    LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.white.opacity(0.4)]), startPoint: .bottom, endPoint: .top)
-                        .clipShape(Circle())
-                )
-            
-            Text(user.pseudonym)
-                .font(.title)
-                .fontWeight(.bold)
-            
-            if let firstname = user.firstname, let lastname = user.lastname {
-                Text("\(firstname) \(lastname)")
-                    .font(.subheadline)
+        NavigationView {
+            VStack(spacing: 16) {
+                Spacer() // Pushes content to the top
+                
+                AvatarView(initials: userInitials(user))
+                    .frame(width: 100, height: 100)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.white.opacity(0.4)]), startPoint: .bottom, endPoint: .top)
+                            .clipShape(Circle())
+                    )
+                
+                Text(user.pseudonym)
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                if let firstname = user.firstname, let lastname = user.lastname {
+                    Text("\(firstname) \(lastname)")
+                        .font(.subheadline)
+                }
+                
+                if let email = user.email {
+                    Text("Email: \(email)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                
+                if let iban = user.iban {
+                    Text("IBAN: \(iban)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                
+                Spacer() // Pushes content to the top
             }
-            
-            if let email = user.email {
-                Text("Email: \(email)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
-            
-            if let iban = user.iban {
-                Text("IBAN: \(iban)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-            }
+            .padding()
+            .navigationBarTitle("", displayMode: .inline) // Empty title to remove the default title
+            .navigationBarItems(leading:
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Close")
+                        .foregroundColor(.blue)
+                }
+            )
         }
-        .padding()
     }
     
     private func userInitials(_ user: User) -> String {
