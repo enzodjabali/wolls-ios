d45478260f535ac1cd5c5ff1f9746bdd2a2f9cee
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-06-19 12:30:38 +0300

    feat(#26): displaying the user details in boxes

--- goodfriends/Views/UserViews/UserDetailView.swift
+++ goodfriends/Views/UserViews/UserDetailView.swift
@@ -9,107 +9,152 @@ struct UserDetailView: View {
         NavigationView {
             VStack(spacing: 16) {
                 Spacer() // Pushes content to the top
-                
+
                 AvatarViewProfile(initials: userInitials(user))
                     .frame(width: 200, height: 200)
                     .background(
                         LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.8), Color.white.opacity(0.4)]), startPoint: .bottom, endPoint: .top)
                             .clipShape(Circle())
                     )
-                
+
                 if let firstname = user.firstname, let lastname = user.lastname {
                     Text("\(firstname) \(lastname)")
                         .font(.title)
                         .fontWeight(.bold)
                 }
-                
+
                 Text("@" + user.pseudonym)
                     .font(.subheadline)
                     .foregroundColor(.gray)
                     .padding(.top, -10)
-                
-                if user.is_administrator == true {
-                    Text("Administrator")
-                        .font(.subheadline)
-                        .foregroundColor(.blue)
-                        .padding(5)
-                        .background(Color.blue.opacity(0.1))
-                        .cornerRadius(5)
-                } else {
-                    Text("Member")
-                        .font(.subheadline)
-                        .foregroundColor(.blue)
-                        .padding(5)
-                        .background(Color.blue.opacity(0.1))
-                        .cornerRadius(5)
-                }
-                
-                if let email = user.email {
-                    Text("Email: \(email)")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                
-                if let iban = user.iban {
-                    if !iban.isEmpty {
-                        Text("IBAN: \(iban)")
-                            .font(.subheadline)
+
+                VStack(alignment: .leading, spacing: 10) {
+                    // Role Section
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Role")
+                            .font(.headline)
                             .foregroundColor(.gray)
-                    } else {
-                        Text("IBAN not provided by the user.")
-                            .font(.subheadline)
-                            .foregroundColor(.orange)
-                    }
-                }
-                
-                if let iban = user.iban, !iban.isEmpty {
-                    Button(action: {
-                        UIPasteboard.general.string = iban
-                        copiedIBAN = true
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            copiedIBAN = false
+                        HStack {
+                            if user.is_administrator == true {
+                                Text("Administrator")
+                                    .font(.subheadline)
+                                    .foregroundColor(.blue)
+                                    .padding(5)
+                                    .background(Color.blue.opacity(0.1))
+                                    .cornerRadius(5)
+                            } else {
+                                Text("Member")
+                                    .font(.subheadline)
+                                    .foregroundColor(.blue)
+                                    .padding(5)
+                                    .background(Color.blue.opacity(0.1))
+                                    .cornerRadius(5)
+                            }
+                            Spacer()
                         }
-                    }) {
-                        Text("Copy IBAN")
-                            .foregroundColor(.blue)
+                        .padding()
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .cornerRadius(10)
                     }
-                    .alert(isPresented: $copiedIBAN) {
-                        Alert(
-                            title: Text("IBAN Copied"),
-                            dismissButton: .default(Text("OK"))
-                        )
-                    }
-                }
-                
-                if let balance = user.balance {
-                    HStack {
-                        if balance < 0 {
-                            Rectangle()
-                                .fill(Color.red.opacity(0.8)) // Set opacity here
-                                .cornerRadius(5) // Set corner radius here
-                                .frame(width: max(CGFloat(abs(balance) / maxBalance) * maxBarWidth, 80), height: 25) // Ensure minimum width
-                                .overlay(
-                                    Text("\(String(format: "%.2f", balance)) €")
-                                        .font(.subheadline)
-                                        .foregroundColor(.white)
-                                )
-                                .padding(.horizontal, 5) // Add horizontal padding for better spacing
-                        }
-                        if balance > 0 {
-                            Rectangle()
-                                .fill(Color.green.opacity(0.8)) // Set opacity here
-                                .cornerRadius(5) // Set corner radius here
-                                .frame(width: max(CGFloat(balance / maxBalance) * maxBarWidth, 80), height: 25) // Ensure minimum width
-                                .overlay(
-                                    Text("\(String(format: "%.2f", balance)) €")
-                                        .font(.subheadline)
-                                        .foregroundColor(.white)
-                                )
-                                .padding(.horizontal, 5) // Add horizontal padding for better spacing
+
+                    // Email Section
+                    if let email = user.email {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Email")
+                                .font(.headline)
+                                .foregroundColor(.gray)
+                            HStack {
+                                Text(email)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            .padding()
+                            .background(Color(UIColor.secondarySystemBackground))
+                            .cornerRadius(10)
                         }
                     }
+
+                    // IBAN Section
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("IBAN")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        HStack {
+                            if let iban = user.iban, !iban.isEmpty {
+                                Text(iban)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            } else {
+                                Text("IBAN not provided by the user.")
+                                    .font(.subheadline)
+                                    .foregroundColor(.orange)
+                            }
+                            Spacer()
+                            if let iban = user.iban, !iban.isEmpty {
+                                Button(action: {
+                                    UIPasteboard.general.string = iban
+                                    copiedIBAN = true
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                        copiedIBAN = false
+                                    }
+                                }) {
+                                    Text("Copy")
+                                        .foregroundColor(.blue)
+                                }
+                                .alert(isPresented: $copiedIBAN) {
+                                    Alert(
+                                        title: Text("IBAN Copied"),
+                                        dismissButton: .default(Text("OK"))
+                                    )
+                                }
+                            }
+                        }
+                        .padding()
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .cornerRadius(10)
+                    }
+
+                    // Balance Section
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Balance")
+                            .font(.headline)
+                            .foregroundColor(.gray)
+                        HStack {
+                            if let balance = user.balance {
+                                if balance < 0 {
+                                    Rectangle()
+                                        .fill(Color.red.opacity(0.8)) // Set opacity here
+                                        .cornerRadius(5) // Set corner radius here
+                                        .frame(width: max(CGFloat(abs(balance) / maxBalance) * maxBarWidth, 80), height: 25) // Ensure minimum width
+                                        .overlay(
+                                            Text("\(String(format: "%.2f", balance)) €")
+                                                .font(.subheadline)
+                                                .foregroundColor(.white)
+                                        )
+                                        .padding(.horizontal, 5) // Add horizontal padding for better spacing
+                                } else {
+                                    Rectangle()
+                                        .fill(Color.green.opacity(0.8)) // Set opacity here
+                                        .cornerRadius(5) // Set corner radius here
+                                        .frame(width: max(CGFloat(balance / maxBalance) * maxBarWidth, 80), height: 25) // Ensure minimum width
+                                        .overlay(
+                                            Text("\(String(format: "%.2f", balance)) €")
+                                                .font(.subheadline)
+                                                .foregroundColor(.white)
+                                        )
+                                        .padding(.horizontal, 5) // Add horizontal padding for better spacing
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding()
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .cornerRadius(10)
+                    }
                 }
-                
+                .padding([.leading, .trailing], 20)
+
                 Spacer() // Pushes content to the top
             }
             .padding()
@@ -124,13 +169,13 @@ struct UserDetailView: View {
             )
         }
     }
-    
+
     private func userInitials(_ user: User) -> String {
         let firstInitial = user.firstname?.first ?? "?"
         let lastInitial = user.lastname?.first ?? "?"
         return "\(firstInitial)\(lastInitial)"
     }
-    
+
     private var maxBalance: Float {
         return 1000 // Replace with your logic to determine the maximum balance
     }
