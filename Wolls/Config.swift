7317361fa454efa710094b2bebc43ee71b0294b1
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-07-21 22:46:38 +0200

    tech(#36): setting up the v1.0.1-beta version

--- Wolls/Config.swift
+++ Wolls/Config.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 struct API {
-    static let baseURL = "http://127.0.0.1:3000"
+    static let baseURL = "https://a4f5bce3-b318-47d5-8c9b-63affc46a180.wolls.app"
 }
 
 struct AppInfo {
